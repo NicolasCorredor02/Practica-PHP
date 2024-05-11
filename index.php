9ae32bd221022653f2ecd5eb41c37ca587773ed0
NicolasCorredor02 <nicolas.d02@outlook.com>
2024-05-11 10:09:03 -0500

    Se añade index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<h1>
+<?php
+
+echo 'Hola mundo';
+
+?>
+</h1>
+<style>
+    :root{
+        color-scheme: ligth dark;
+    }
+
+    body{
+        display: grid;
+        place-content: center;
+    }
+</style>
